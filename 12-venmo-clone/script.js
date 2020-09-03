940216a6a07575fb3c58b544ba1f8b448fce02d5
william-zhang-fullstack <william.zhang.fullstack@gmail.com>
2020-09-03 11:48:08 -0400

    added radio btn functionality in navbar btns

--- 12-venmo-clone/script.js
+++ 12-venmo-clone/script.js
@@ -1,3 +1,4 @@
+const navBtns   = document.querySelectorAll('#nav-menu button');
 const burgerBtn = document.getElementById('burger-btn');
 const sidebar   = document.querySelectorAll('.sidebar')[0];
 const overlay   = document.querySelectorAll('.overlay')[0];
@@ -6,6 +7,17 @@ const expandSidebar = event => {
     sidebar.style.width = '83%';
     overlay.style.opacity = '0.7';
 };
+
+navBtns.forEach(btn => btn.addEventListener("click", event => {
+    navBtns.forEach(btn => {
+        if (btn === event.target) {
+            btn.classList.add('selected');
+        } else {
+            console.log('removed');
+            btn.classList.remove('selected');
+        }
+    });
+}));
 
 burgerBtn.addEventListener('click', expandSidebar);
 
